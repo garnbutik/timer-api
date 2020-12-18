ab16a6b5dc7ebd4eb61a4cb2bd3eddadc3683e1b
Fredrik Harnevik <fredrik.harnevik@gmail.com>
2020-12-18 14:48:03 +0100

    adding login tests

--- src/test/java/com/garnbutik/controllers/ControllerTest.java
+++ src/test/java/com/garnbutik/controllers/ControllerTest.java
@@ -1,17 +1,21 @@
 package com.garnbutik.controllers;
+
+import com.garnbutik.model.Credentials;
 import com.garnbutik.model.User;
 import com.garnbutik.security.JwtTokenIssuer;
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import testConfig.WeldJUnit4Runner;
 
 import javax.inject.Inject;
-
 import java.text.MessageFormat;
 
-import static io.restassured.RestAssured.*;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.notNullValue;
 
 @RunWith(WeldJUnit4Runner.class)
 public class ControllerTest {
@@ -27,24 +31,55 @@ public class ControllerTest {
     }
 
     @Test
+    public void whenLoginWithBadCredentials_then401(){
+        given().contentType(ContentType.JSON).body(getBadCredentials()).log().all()
+                .when().post("login")
+                    .then().log().all().statusCode(401).body("statusCode", equalTo(401));
+    }
+
+    @Test
+    public void whenLoginWithOKCredentials_then200(){
+        given().contentType(ContentType.JSON).body(getOkCredentials()).log().all()
+                .when().post("login")
+                    .then().log().all().statusCode(200).body("token", notNullValue());
+    }
+
+    @Test
     public void whenRequestWithAuthToken_thenOK(){
-        String username = "test";
-        given().log().all().header("Authorization", "Bearer " + getTokenForTestUser())
-                .when().request("GET", MessageFormat.format("users/{0}/", TEST_USERNAME))
-                .then().statusCode(200);
+        given()
+                .log().all()
+                .header(
+                        "Authorization",
+                        "Bearer " + getTokenForTestUser())
+                .pathParam("username", TEST_USERNAME)
+                    .when().get("users/{username}")
+                        .then().statusCode(200);
     }
 
     @Test
     public void whenRequestWithoutAuthToken_then401(){
-
-        given().log().all()
-                .when().request("GET", MessageFormat.format("users/{0}/", TEST_USERNAME))
-                .then().statusCode(401);
+        given().log().all().pathParam("username", TEST_USERNAME)
+                .when().get("users/{username}")
+                    .then().statusCode(401);
     }
 
     private String getTokenForTestUser() {
         User user = new User();
         user.setUsername(ControllerTest.TEST_USERNAME);
         return tokenIssuer.issueToken(user);
+    }
+
+    private static Credentials getOkCredentials() {
+        Credentials okCredentials = new Credentials();
+        okCredentials.setPassword("testtest");
+        okCredentials.setUsername("test");
+        return okCredentials;
+    }
+
+    private static Credentials getBadCredentials() {
+        Credentials badCredentials = new Credentials();
+        badCredentials.setPassword("12345678");
+        badCredentials.setUsername("notAUserName");
+        return badCredentials;
     }
 }
